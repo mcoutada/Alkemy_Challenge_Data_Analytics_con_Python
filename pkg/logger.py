fd0f80a08d3bc63e76f86b876f76e016e65b01d0
Mariano Ismael Coutada <mcoutada@gmail.com>
2022-08-09 20:53:30 -0300

    code org improvement

--- pkg/logger.py
+++ pkg/logger.py
@@ -35,7 +35,7 @@ if not os.path.exists(LOG_DIR):
     os.mkdir(LOG_DIR)
 
 APP_LOGGER_NAME = 'Alkemy_challenge'
-# uncomment after testing
+# TODO: uncomment after testing
 # APP_LOG_FILE_NAME = os.path.join(LOG_DIR, f'{APP_LOGGER_NAME}_{datetime.now():%Y%m%d_%H%M%S-%f}.log')
 APP_LOG_FILE_NAME = os.path.join(LOG_DIR, f'{APP_LOGGER_NAME}.log')
 
@@ -78,43 +78,9 @@ def setup_applevel_logger(logger_name=APP_LOGGER_NAME,
     # Add the console handler to the logger for the messages to be shown on the console
     logger.addHandler(sh)
 
-    def log_unhandled_exception(*exc_info):
-        exc_type, exc_value, exc_traceback = exc_info
-        """
-        Logging uncaught exceptions in Python
-        https://stackoverflow.com/questions/6234405/logging-uncaught-exceptions-in-python/16993115#16993115
-        
-        Args:
-            exc_type (type): Gets the type of the exception being handled (a subclass of BaseException). Example: <class 'ZeroDivisionError'>
-            exc_value (error): Gets the exception instance (an instance of the exception type). Example: division by zero
-            exc_traceback (traceback): Gets a traceback object which encapsulates the call stack at the point where the exception originally occurred. Example: <traceback object at 0x7f8b8b8b8b8>
-        """
-
-        print('\n\n\n')
-        print(exc_info)
-        print('\n')
-        print(exc_type)
-        print('\n')
-        print(exc_value)
-        print('\n')
-        print(exc_traceback)
-        print('\n\n\n')
-
-        # Ignore KeyboardInterrupt so a console python program can exit with Ctrl + C.
-        if issubclass(exc_type, KeyboardInterrupt):
-            sys.__excepthook__(exc_type, exc_value, exc_traceback)
-            return
-
-        # Rely entirely on python's logging module for formatting the exception.
-        logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
-
-    # sys.excepthook(type, value, traceback) prints out a given traceback and exception to sys.stderr
-    # When an exception is raised and uncaught, the interpreter calls sys.excepthook with three arguments:
-    # the exception class, exception instance, and a traceback object.
-    # In an interactive session this happens just before control is returned to the prompt;
-    # in a Python program this happens just before the program exits.
-    # The handling of such top-level exceptions can be customized by assigning another three-argument function to sys.excepthook.
-    sys.excepthook = log_unhandled_exception
+    # sys.excepthook(*exc_info) prints out a given traceback and exception to sys.stderr
+    new_excepthook = lambda *exc_info: log_unhandled_exception(logger, *exc_info)
+    sys.excepthook = new_excepthook
 
     # Set up a file handler if a file name is provided for the messages to be logged to a log file
     if file_name:
@@ -144,7 +110,35 @@ def get_child_logger(module_name, logger_name=APP_LOGGER_NAME):
 
     return logger
 
+def log_unhandled_exception(in_logger, *in_exc_info):
+    exc_type, exc_value, exc_traceback = in_exc_info
+    """
+    Logging uncaught exceptions in Python
+    https://stackoverflow.com/questions/6234405/logging-uncaught-exceptions-in-python/16993115#16993115
+    
+    # When an exception is raised and uncaught, the interpreter calls sys.excepthook with three arguments:
+    # the exception class, exception instance, and a traceback object.
+    # In an interactive session this happens just before control is returned to the prompt;
+    # in a Python program this happens just before the program exits.
+    # The handling of such top-level exceptions can be customized by assigning another three-argument function to sys.excepthook.
+    # sys.excepthook = log_unhandled_exception
+    
+    Args:
+        *exc_info is unpacked to:
+        exc_type (type): Gets the type of the exception being handled (a subclass of BaseException). Example: <class 'ZeroDivisionError'>
+        exc_value (error): Gets the exception instance (an instance of the exception type). Example: division by zero
+        exc_traceback (traceback): Gets a traceback object which encapsulates the call stack at the point where the exception originally occurred. Example: <traceback object at 0x7f8b8b8b8b8>
+    """
 
+    # Ignore KeyboardInterrupt so a console python program can exit with Ctrl + C.
+    if issubclass(exc_type, KeyboardInterrupt):
+        # calls default excepthook
+        # sys.__excepthook__ contains the original values of excepthook
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    # Rely entirely on python's logging module for formatting the exception.
+    in_logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 
 # def debug(func, log):
